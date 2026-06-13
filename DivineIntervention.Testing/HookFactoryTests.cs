7f63d99a39fe476fa8b30efff41a5851a4be80bb
Kyle Givler <xvegan88x@gmail.com>
2026-06-13 16:07:04 -0400

    Re-organize and add initial testing project
    Re-organize and add initial testing project

---
+++ DivineIntervention.Testing/HookFactoryTests.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+namespace DivineIntervention.Tests
+{
+    [TestFixture]
+    public class HookFactoryTests
+    {
+        [Test]
+        public void Test_Framework_Initialization()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
---
+++ DivineIntervention/DivineIntervention/Hooking/Internal/HookDelegates.cs
@@ -0,0 +1,28 @@
+﻿/*
+*  Divine Intervention RimWorld Modding Framework
+*  
+*  Make Mods the Right Way(tm)
+*  
+*  Copyright (c) 2026 Kyle Givler
+*  Licensed under the MIT License.
+*/
+
+namespace DivineIntervention.Hooking;
+
+/// <summary>
+/// Delegate for logic executed before a method runs.
+/// </summary>
+/// <typeparam name="T">The type defining the method.</typeparam>
+/// <param name="instance">The current instance of the object (default if static).</param>
+/// <param name="args">The method arguments array.</param>
+/// <returns>True to continue execution; false to halt the original method.</returns>
+public delegate bool HookPrefix<T>(T instance, object[] args);
+
+/// <summary>
+/// Delegate for logic executed after a method runs, allowing mutation of the return value.
+/// </summary>
+/// <typeparam name="T">The type defining the method.</typeparam>
+/// <param name="instance">The current instance of the object (default if static).</param>
+/// <param name="args">The method arguments array.</param>
+/// <param name="result">The current result of the method, passed by reference.</param>
+public delegate void HookPostfix<T>(T instance, object[] args, ref object result);
